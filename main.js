0db8e8c348e891b8b963c9094dec49caeaa292b0
walco005 <walco005@morris.umn.edu>
2014-11-27 01:00:16 -0600

    Changed to add a random string to the rightmost value in the divs when pressing the left arrow.

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ $(document).ready(function() {
 
 $(document).keydown(function(key) {
 
-//    //When the left arrow key is pressed, removes the rightmost value in word1 and puts it in word2.
-//    if(key.keyCode == 37) {
-//        var w1 = $("#word1").text();
-//        var w2 = $("#word2").text();
-//        var last1 = w1.substr(w1.length - 1);
-//        var newW1 = w1.slice(0,-1);
-//        var newW2 = last1 + w2;
-//        $("#word1").text(newW1);
-//        $("#word2").text(newW2);
-//    };
-//
-//    //When the right arrow key is pressed, removes the leftmost value in word2 and puts it in word1.
-//    if(key.keyCode == 39) {
-//        var w1 = $("#word1").text();
-//        var w2 = $("#word2").text();
-//        var first2 = w2.charAt(0);
-//        var newW2 = w2.slice(1,w2.length);
-//        var newW1 = w1 + first2;
-//        $("#word1").text(newW1);
-//        $("#word2").text(newW2);
-//    };
+    //When the down arrow key is pressed, removes the rightmost value in word1 and puts it in word2.
+    if(key.keyCode == 40) {
+        var w1 = $("#word1").text();
+        var w2 = $("#word2").text();
+        var last1 = w1.substr(w1.length - 1);
+        var newW1 = w1.slice(0,-1);
+        var newW2 = last1 + w2;
+        $("#word1").text(newW1);
+        $("#word2").text(newW2);
+    };
+
+    //When the up arrow key is pressed, removes the leftmost value in word2 and puts it in word1.
+    if(key.keyCode == 38) {
+        var w1 = $("#word1").text();
+        var w2 = $("#word2").text();
+        var first2 = w2.charAt(0);
+        var newW2 = w2.slice(1,w2.length);
+        var newW1 = w1 + first2;
+        $("#word1").text(newW1);
+        $("#word2").text(newW2);
+    };
 
     //Checks if the character typed is the first character in the span "toType" and moves it to the span "typed"
     var charToType = $("#toType").text().charAt(0);
@@ -38,6 +38,8 @@ $(document).keydown(function(key) {
         $("#toType").text(type);
     };
 
+    //Test array of strings
+    var arr = ["these","words","are","really","super","cool","and","kinda","random","yep",];
     //Right arrow press
     if(key.keyCode == 39) {
         $("#notTyped2").text($("#notTyped1").text());
@@ -52,19 +54,8 @@ $(document).keydown(function(key) {
         $("#typed1").text($("#toType1").text());
         $("#toType1").text($("#notTyped1").text());
         $("#notTyped1").text($("#notTyped2").text());
+        var rand = Math.floor(Math.random() * 11);
+        $("#notTyped2").text(arr[rand]);
     };
 
-//    function moveDivs(direction) {
-//        if(direction = "left") {
-//            $("#typed2").text($("#typed1").text());
-//            $("#typed1").text($("#toType1").text());
-//            $("#toType1").text($("#notTyped1").text());
-//            $("#notTyped1").text($("#notTyped2").text());
-//        } else if(direction = "right") {
-//            $("#notTyped1").text($("#notTyped2").text());
-//            $("#toType1").text($("#notTyped1").text());
-//            $("#typed1").text($("#toType1").text());
-//            $("#typed2").text($("#notTyped1").text());
-//        }
-//     }
 });
