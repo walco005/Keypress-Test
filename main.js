cc1939c4b6bcbe7d651d5b01d70d0f83d94d11e9
walco005 <walco005@morris.umn.edu>
2014-11-26 23:29:16 -0600

    Implemented typing a word displayed, turning the characters green for each character you type and added html headers.

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
 });
 
 $(document).keydown(function(key) {
-    //Grabs the keyCode of the key pressed & displays it in the typed div
-    $("#typed").html(key.keyCode);
 
     //When the left arrow key is pressed, removes the rightmost value in word1 and puts it in word2.
     if(key.keyCode == 37) {
@@ -23,9 +21,20 @@ $(document).keydown(function(key) {
         var w2 = $("#word2").text();
         var first2 = w2.charAt(0);
         var newW2 = w2.slice(1,w2.length);
-        console.log("New Word 2: " + newW2);
         var newW1 = w1 + first2;
         $("#word1").text(newW1);
         $("#word2").text(newW2);
+    };
+
+    //Checks if the character typed is the first character in the span "toType" and moves it to the span "typed"
+    var charToType = $("#toType").text().charAt(0);
+    var keyPressed = String.fromCharCode(key.keyCode);
+    if(keyPressed.toLowerCase() == charToType) {
+        var typed = $("#typed").text();
+        typed += charToType;
+        $("#typed").text(typed);
+        var type = $("#toType").text();
+        type = type.substr(1, type.length);
+        $("#toType").text(type);
     };
 });
