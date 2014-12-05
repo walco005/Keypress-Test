2238d89aee8c78c9ef0484b0560f4cadb6cae2ba
walco005 <walco005@morris.umn.edu>
2014-12-05 14:29:43 -0600

    Added functionality to get the word length and a bonus based on the word, as well as misc. console.log tests

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 //Test array of strings
-var arr = ["these","words","are","really","super","cool","and","kinda","random","yep",];
+var arr = ["these","words","are","really","super","cool","and","kinda","random","yep", "her's", "their's", "them's", "these's"];
+var wordCount = 0;
 
 $(document).ready(function() {
     initializeWords();
+    printWordLength();
 });
 
-
+//A test function to print out "number" amount of random integers from 0 to the length of the array arr.
 function printRandoms(number) {
     var i = 0;
     while(i < number) {
         i++;
         var rand = Math.floor(Math.random() * arr.length);
-        console.log("Random Number #" + i + ": " + rand);
+    }
+};
+
+//Gives the "word bonus" of a word based on it's length. The average word has a length of 5 characters, so it divides the length by 5.
+function wordBonus(length) {
+    var bonusLength = length/5;
+    return bonusLength;
+};
+
+//Goes through an array printing the word, word length, and the word bonus.
+function printWordLength() {
+    var i = 0;
+    while (i < arr.length) {
+        console.log("Number " + (i));
+        var length = arr[i].length
+        console.log("Word: " + arr[i] + ", Length: " + length + ", Bonus: " + wordBonus(length));
+        console.log("");
+        i++;
     }
 };
 
@@ -27,28 +46,6 @@ function initializeWords() {
 
 $(document).keydown(function(key) {
 
-//    //When the down arrow key is pressed, removes the rightmost value in word1 and puts it in word2.
-//    if(key.keyCode == 40) {
-//        var w1 = $("#word1").text();
-//        var w2 = $("#word2").text();
-//        var last1 = w1.substr(w1.length - 1);
-//        var newW1 = w1.slice(0,-1);
-//        var newW2 = last1 + w2;
-//        $("#word1").text(newW1);
-//        $("#word2").text(newW2);
-//    };
-//
-//    //When the up arrow key is pressed, removes the leftmost value in word2 and puts it in word1.
-//    if(key.keyCode == 38) {
-//        var w1 = $("#word1").text();
-//        var w2 = $("#word2").text();
-//        var first2 = w2.charAt(0);
-//        var newW2 = w2.slice(1,w2.length);
-//        var newW1 = w1 + first2;
-//        $("#word1").text(newW1);
-//        $("#word2").text(newW2);
-//    };
-
     //Checks if the character typed is the first character in the span "toType" and moves it to the span "typed"
     var charToType = $("#toType").text().charAt(0);
     var keyPressed = String.fromCharCode(key.keyCode);
@@ -61,7 +58,8 @@ $(document).keydown(function(key) {
         $("#toType").text(type);
     };
 
-    //Checks if space is being pressed as well as if the span #toType is empty, if so it triggers divMove(left).
+    //Checks if space is being pressed as well as if the span #toType is empty, if so it moves the contents of each div to the left and populates
+    //   the rightmost div with a random word from the array.
     if((key.keyCode == 0 || key.keyCode == 32) && $("#toType").text() == "") {
             $("#typed2").text($("#typed1").text());
             $("#typed1").text($("#typed").text());
